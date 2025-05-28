7ec3f1a65d0ce2ac0f3ef8f9000074c5367db419
jared <20243ds034@utez.edu.mx>
2025-05-28 08:44:56 -0600

    presentacion agregada

--- U1_TI_06_Computadora/src/Main.java
+++ U1_TI_06_Computadora/src/Main.java
@@ -9,6 +9,6 @@ public class Main {
         computadora.encender();
         computadora.apagar();
         System.out.println(computadora.getModelo());
-
+        System.out.println("==============================");
     }
 }
