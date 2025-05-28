13114065c3482711bda9afeaece15e2ffbc82305
nikilauda10 <20243ds050@utez.edu.mx>
2025-05-28 08:47:47 -0600

    Mas presentacion agregada y ya me salio una nueva hernia

--- U1_TI_06_Computadora/src/Main.java
+++ U1_TI_06_Computadora/src/Main.java
@@ -4,11 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         Computadora computadora = new Computadora();
+        System.out.println("==============================");
         computadora.setModelo("MacBook Pro ");
         computadora.encender();
         computadora.encender();
         computadora.apagar();
         System.out.println(computadora.getModelo());
         System.out.println("==============================");
+
     }
 }
