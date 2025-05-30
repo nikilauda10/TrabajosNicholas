5adb67811e89a97143722431183fa96260757c15
nikilauda10 <20243ds050@utez.edu.mx>
2025-05-29 18:54:24 -0600

    sigo practicando git pero quiero que el commit salga solo en la carpeta de practica

--- practica/cuadrado/src/App.java
+++ practica/cuadrado/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Square sq = new Square();
         sq.setSide(10);
+       System.out.println("____________________________________________________");
         System.out.println("El area del cuadrado representado es: " + sq.getArea());
         System.out.println("El perimetro es: " + sq.getPerimeter());
 
